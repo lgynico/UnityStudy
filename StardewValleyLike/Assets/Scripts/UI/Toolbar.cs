36ec0fa28ad6e1942ef08ebdf556caf5d24bfce1
linguoying <935381009@qq.com>
2024-09-05 18:40:53 +0800

    完善 Toolba

--- StardewValleyLike/Assets/Scripts/UI/Toolbar.cs
+++ StardewValleyLike/Assets/Scripts/UI/Toolbar.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Toolbar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<ToolbarSlot> slots;
+
+    private ToolbarSlot selected;
+
+
+    private void Start()
     {
-        
+        InitUI();
+        UpdateUI();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.anyKeyDown)
+        {
+            for (var i = KeyCode.Alpha1; i <= KeyCode.Alpha9; i++)
+            {
+                if (Input.GetKeyDown(i))
+                {
+                    selected?.Unselect();
+
+                    int index = i - KeyCode.Alpha1;
+                    slots[index].Select();
+                    selected = slots[index];
+                }
+            }
+        }
+    }
+
+    private void InitUI()
+    {
+        slots = new List<ToolbarSlot>(new ToolbarSlot[9]);
+        var slotsGrid = transform.Find("Slots");
+        for (int i = 0; i < slotsGrid.childCount; i++)
+        {
+            slots[i] = slotsGrid.GetChild(i).GetComponent<ToolbarSlot>();
+        }
+    }
+
+    private void UpdateUI()
+    {
+        for (int i = 0; i < InventoryManager.Instance.toolbar.slots.Count; i++)
+        {
+            slots[i].SetData(InventoryManager.Instance.toolbar.slots[i]);
+        }
     }
 }
--- StardewValleyLike/Assets/Scripts/UI/ToolbarSlot.cs
+++ StardewValleyLike/Assets/Scripts/UI/ToolbarSlot.cs
@@ -1,9 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ToolbarSlot : Slot
 {
     public Sprite unselected;
     public Sprite selected;
+
+    private Image background;
+
+    private void Start()
+    {
+        background = GetComponent<Image>();
+    }
+
+    public void Select()
+    {
+        background.sprite = selected;
+    }
+
+    public void Unselect()
+    {
+        background.sprite = unselected;
+    }
 }
